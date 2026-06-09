e032e04b9d307abb2826bde129466d4e17582352
vit_nik <shcherbakov.vitali@gmail.com>
2026-06-09 10:08:34 +0200

    10. AWS IaC with Terraform: Move Resources between State Files

---
+++ tf_code_1/resources.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_policy" "custom_policy" {
+  name        = "resource-move-demo-policy"
+  description = "Custom role with limited permissions"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = ["ec2:*", "s3:*"], # Replace with specific actions
+        Resource = "*"
+      }
+    ]
+  })
+}
---
+++ tf_code_2/resources.tf
@@ -0,0 +1,19 @@
+﻿resource "aws_iam_policy" "custom_policy" {
+  name        = "resource-move-demo-policy"
+  path        = "/"
+  description = "Custom role with limited permissions"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:*",
+          "s3:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
